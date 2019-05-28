0747673da83a8deb8bc6c3db83a0096db26d60a1
Aufar Laksana <alaksana96@gmail.com>
2019-05-28 17:47:42 +0100

    Custom Messages: - Added Custom Messages to subscribe to the detectiondirections output from people_tracker - Build new dlls - Tested communication on Unity Editor

---
+++ Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/DetectionAndDirectionSubscriber.cs
@@ -0,0 +1,19 @@
+﻿using RosSharp.RosBridgeClient.Messages.HoloFyp;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    [RequireComponent(typeof(RosConnector))]
+    public class DetectionAndDirectionSubscriber : Subscriber<Messages.HoloFyp.DetectionAndDirection>
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void ReceiveMessage(DetectionAndDirection message)
+        {
+            Debug.Log("Received Something");
+        }
+    }
+}
